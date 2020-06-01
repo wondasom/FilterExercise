7417d33173737b0b0d1a696ec82414c957a25096
Dasom Won <59657188+wondasom@users.noreply.github.com>
2020-06-01 15:22:24 +0200

    create and import json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./styles.css";
 
+import restaurants from "./restaurants.json";
+
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>I am practicing with small json file to try filter feature!</h2>
+      {JSON.stringify(restaurants)}
     </div>
   );
 }
